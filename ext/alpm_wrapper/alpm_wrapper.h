b046e339b513173f11d711bb90f434d40e54ac72
NightFeather <ss900328@gmail.com>
2017-02-06 17:31:03 +0800

    Extract common parts to a common.h

--- ext/alpm_wrapper/alpm_wrapper.h
+++ ext/alpm_wrapper/alpm_wrapper.h
@@ -1,12 +1,14 @@
 #ifndef _ALPM_WRAPPER_H_
 #define _ALPM_WRAPPER_H_
 
+#include "common.h"
+#include "alpm_wrapper.h"
+#include "alpm_handler.h"
+#include "alpm_package.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <alpm.h>
-#include <ruby.h>
 
 void Init_alpm_wrapper(void);
 
---
+++ ext/alpm_wrapper/common.h
@@ -0,0 +1,24 @@
+#ifndef _ALPM_WRAPPER_COMMON_H_
+#define _ALPM_WRAPPER_COMMON_H_
+
+#include <alpm.h>
+#include <ruby.h>
+
+#define RB_COMMON_ALLOC(name)\
+static VALUE name ## _allocate(VALUE klass){\
+  rb_ ## name * handler_ptr = NULL;\
+  return Data_Make_Struct( klass, rb_ ## name, NULL, name ## _deallocate, handler_ptr);\
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* define the common namespace */
+extern VALUE mALPM;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
